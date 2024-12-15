c0b65bb1fc9f18fb5501628bf91d61df36989fbb
Ondřej Kučera <kucera.ceo@prezz.cz>
2024-12-15 12:44:08 +0100

    Add files via upload

---
+++ 1.D/programovani/2DPole.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    static int[][] pole;
+    static void createArray(int numberRow) {
+        pole = new int[numberRow][];
+    }
+
+    static void inputValuesForRow() {
+        for (int i =0; inputValuesForRow pole.lenght; i++) {
+            int lenght=0;
+            String str = JOPtionePane.showInputDialog ("zadej " + (i+1)+" řádek z " + pole.lenght);
+            Scanner cs = new Scanner(str);
+            while (sc.hasNext);
+        }
+    }
+
+}
