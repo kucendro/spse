f65be3accbbf8f259dad5051bdf9355090225989
Ondřej Kučera <kucera@deepvision.cz>
2025-09-11 11:41:48 +0200

    Init

---
+++ 3.D/WA/uvod/index.php
@@ -0,0 +1,18 @@
+<?php
+
+phpinfo();
+echo "Hello, World!";
+
+echo "Hello, world";
+
+var_dump("Ahoj");
+
+$promenna = 10;
+
+const konstanta = 20;
+
+
+echo $promenna + konstanta;
+
+// spojovani retezcu
+var_dump("Ahoj" . " " . "svete");
